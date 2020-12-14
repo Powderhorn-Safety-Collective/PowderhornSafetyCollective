071c9ecb7ba5673b43dcc84e2ebe3f393f27f8e4
Adam Boerhave <arboerhave@gmail.com>
2020-12-14 11:36:35 -0600

    added database.sql file

--- database.sql
+++ database.sql
@@ -1,10 +1,100 @@
-
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
-CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+CREATE TABLE "users" (
+	"id" serial NOT NULL,
+	"handle" varchar(255) NOT NULL UNIQUE,
+	"first_name" varchar(255) NOT NULL,
+	"last_name" varchar(255) NOT NULL,
+	"address" varchar(255),
+	"email" varchar(255) NOT NULL,
+	"phone" varchar(255) NOT NULL,
+	"adult" BOOLEAN NOT NULL,
+	"patrol" varchar(255),
+	CONSTRAINT "users_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
 );
+
+
+
+CREATE TABLE "incidents" (
+	"id" serial NOT NULL,
+	"type" varchar(511) NOT NULL,
+	"notes" varchar(1000),
+	"location" varchar(255) NOT NULL,
+	"time_submitted" TIMESTAMP NOT NULL,
+	"status" varchar(255) NOT NULL,
+	"view_publicly" BOOLEAN,
+	"responder_notes" varchar(1000),
+	"duplicate_entry" BOOLEAN,
+	"client_id" varchar(255) NOT NULL,
+	CONSTRAINT "incidents_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "incident_followers" (
+	"id" serial NOT NULL,
+	"incident_id" integer NOT NULL,
+	"user_id" integer NOT NULL,
+	CONSTRAINT "incident_followers_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "roles" (
+	"id" serial NOT NULL,
+	"type" varchar(255) NOT NULL,
+	CONSTRAINT "roles_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "users_roles" (
+	"id" serial NOT NULL,
+	"user_id" integer NOT NULL,
+	"role_id" integer NOT NULL,
+	CONSTRAINT "users_roles_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "users_skills" (
+	"id" serial NOT NULL,
+	"user_id" integer NOT NULL,
+	"skill_id" integer NOT NULL,
+	CONSTRAINT "users_skills_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "skills" (
+	"id" serial NOT NULL,
+	"type" varchar(255) NOT NULL,
+	CONSTRAINT "skills_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+
+ALTER TABLE "incident_followers" ADD CONSTRAINT "incident_followers_fk0" FOREIGN KEY ("incident_id") REFERENCES "incidents"("id");
+ALTER TABLE "incident_followers" ADD CONSTRAINT "incident_followers_fk1" FOREIGN KEY ("user_id") REFERENCES "users"("id");
+
+
+ALTER TABLE "users_roles" ADD CONSTRAINT "users_roles_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("id");
+ALTER TABLE "users_roles" ADD CONSTRAINT "users_roles_fk1" FOREIGN KEY ("role_id") REFERENCES "roles"("id");
+
+ALTER TABLE "users_skills" ADD CONSTRAINT "users_skills_fk0" FOREIGN KEY ("user_id") REFERENCES "users"("id");
+ALTER TABLE "users_skills" ADD CONSTRAINT "users_skills_fk1" FOREIGN KEY ("skill_id") REFERENCES "skills"("id");
+

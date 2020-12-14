7c574aa610ad85a5f5d36b009562b7c90954539c
Kelly Danger <kellyanndanger@gmail.com>
2020-12-14 14:06:00 -0600

    added text disp of patrol and formatted

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -18,9 +18,16 @@ const Nav = (props) => {
 
   return (
     <div className="nav">
+      <div className="titleContainer">
       <Link to="/home">
-        <h2 className="nav-title">Prime Solo Project</h2>
+        <h2 className="nav-title">Powderhorn Safety Collective</h2>
       </Link>
+      <div className="patrolDisplay">
+        <h2>### people are on patrol</h2>
+        <h2>### people are on call</h2>
+      </div>
+      </div>
+
       <div className="nav-right">
         <Link className="nav-link" to={loginLinkData.path}>
           {/* Show this link if they are logged in or not,

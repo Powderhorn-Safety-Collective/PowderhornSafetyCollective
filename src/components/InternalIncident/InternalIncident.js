0839d26d4d9d280ece44c24dc37f7a6d13416371
Adam Boerhave <arboerhave@gmail.com>
2020-12-16 21:44:37 -0600

    textarea displays text saved in db and prints what's in box to console. need to send to db tmrw

---
+++ src/components/InternalIncident/InternalIncident.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import mapStoreToProps from '../../redux/mapStoreToProps';
+import './InternalIncident.css'
+
+// This component is going to be the card display for the incident
+// that appears and is consumed by the Community Page Component
+class InternalIncident extends Component {
+
+  state = {
+    publicText : this.props.incident.text_for_public_display || ''
+  }
+  // function to render active or inactive on incident card
+  renderStatus = ( active) => {
+    if ( active === true) {
+      return <p className="active">Active</p>
+    }
+    else if ( active === false) {
+      return <p className="active">Inactive</p>
+    }
+  }
+
+  // function to render time associated with incident
+  renderTime = ( time) => {
+    let timeHour = Number(time.slice(11,13));
+    let timeMorningEvening = 'a.m.';
+    if (timeHour > 12) {
+      timeHour -= 12;
+      timeMorningEvening = 'p.m.';
+    }
+    else if (timeHour == 0) {
+      timeHour = 12;
+    }
+    let timeMinute = time.slice(14, 16);
+    let month = Number(time.slice(5,7));
+    let day = Number(time.slice(8,10));
+    let year = Number(time.slice(0,4));
+    let displayTime = timeHour + ':' + timeMinute + ' ' + timeMorningEvening + ' ' + month + '/' + day + '/' + year;
+    return <p>{displayTime}</p>
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      publicText: event.target.value
+    })
+  }
+
+  handlePublicTextSave = () => {
+    console.log('this.state.publicText', this.state.publicText);
+    
+  }
+
+  render() {
+    return (
+      <div className="internalModule">
+        <h3>Incident Number: {this.props.incident.client_id}</h3>
+        {JSON.stringify(this.props.incident)}
+        {this.renderStatus( this.props.incident.active)}
+        {/* username toggle here */}
+        <p>Submitted by: {this.props.incident.username}</p>
+        {/* time/date toggle here */}
+        {this.renderTime( this.props.incident.time_submitted)}
+        {/* location toggle goes here */}
+        <p>Location: {this.props.incident.location}</p>
+        {/* type toggle goes here */}
+        <p>Type: {this.props.incident.type}</p>
+        {/* toggle for user notes goes here */}
+        <p>User Notes: {this.props.incident.notes}</p>
+        <label htmlFor="publicText">
+          Text to be displayed at beginning of public post:
+        </label>
+        <br/>
+        <textarea 
+          id="publicText"
+          placeholder="Text for public post"
+          onChange={(event) => this.handleChange(event)}
+          value={this.state.publicText}
+        />
+        <br/>
+        <button 
+          onClick={this.handlePublicTextSave} 
+          className="btn"
+        >
+          Save Public Display Text
+        </button>
+      </div>
+    );
+  }
+}
+
+export default connect(mapStoreToProps)(InternalIncident);
--- src/components/MemberPage/MemberPage.js
+++ src/components/MemberPage/MemberPage.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import LogOutButton from '../LogOutButton/LogOutButton';
 import PatrolItem from '../PatrolItem/PatrolItem.js';
 import OnCallItem from '../OnCallItem/OnCallItem';
 import mapStoreToProps from '../../redux/mapStoreToProps';
+import './MemberPage.css';
+import InternalIncident from '../InternalIncident/InternalIncident';
 
 // member page will be what PSC members see when they log in
 class MemberPage extends Component {
@@ -23,27 +24,39 @@ class MemberPage extends Component {
         <h1>FOR THE NAV BAR</h1>
         <h1 id="welcome">Welcome PSC Volunteer, {this.props.store.user.username}!</h1>
         <p>Your ID is: {this.props.store.user.id}</p>
+        <main className="row">
+          <div className="memberLeft"> {/* left stuff for incidents */}
+            
+            <h1> All Incidents</h1>
 
-        <h1> FOR THE INCIDENT COMPONENT</h1>
-        <div class="onPatrolDisplay">
-          <h2>Members on patrol: </h2>
-          <ul>  
-            {this.props.store.patrolReducer.map((patroller) => {
-              return <PatrolItem patroller={patroller}/>
+            {this.props.store.incidentReducer.map((incident) => {
+              return <InternalIncident incident = {incident} />
             })}
-          </ul>
-        </div>
-        <div class="onCallDisplay">
-          <h2>Members on call: </h2>
-            <ul>  
-            {this.props.store.onCallReducer.map((onCall) => {
-              return <OnCallItem onCall={onCall}/>
-            })}
-            </ul>
-        </div>
+          </div> {/* end of left incident stuff */}
 
-        <LogOutButton className="log-in" />
-      </div>
+          {/* right on patrol / on call display */}
+          <div className="memberRight">
+            {/* on patrol display */}
+            <div class="onPatrolDisplay">
+              <h2>Members on patrol: </h2>
+              <ul>  
+                {this.props.store.patrolReducer.map((patroller) => {
+                  return <PatrolItem patroller={patroller}/>
+                })}
+              </ul>
+            </div>
+            {/* on call display */}
+            <div class="onCallDisplay">
+              <h2>Members on call: </h2>
+              <ul>  
+                {this.props.store.onCallReducer.map((onCall) => {
+                  return <OnCallItem onCall={onCall}/>
+                })}
+              </ul>
+            </div> {/* end on call */}
+          </div> {/* end of right on patrol / on call display */}
+        </main>
+      </div> // end of page
     );
   }
 }

607a4969cf9369a60429c76fcf9cd9842d426559
Adam Boerhave <arboerhave@gmail.com>
2020-12-17 20:57:02 -0600

    toggle switches with local state for all five cond rendering pieces

--- src/components/InternalIncident/InternalIncident.js
+++ src/components/InternalIncident/InternalIncident.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 import './InternalIncident.css'
+import ToggleSwitch from '../ToggleSwitch/ToggleSwitch';
 
 // This component is going to be the card display for the incident
 // that appears and is consumed by the Community Page Component
 class InternalIncident extends Component {
 
   state = {
-    publicText : this.props.incident.text_for_public_display || ''
+    publicText : this.props.incident.text_for_public_display || '',
+    username_public: this.props.incident.username_public,
+    timedate_public: this.props.incident.timedate_public,
+    location_public: this.props.incident.location_public,
+    type_public: this.props.incident.type_public,
+    user_notes_public: this.props.incident.user_notes_public
   }
+
   // function to render active or inactive on incident card
   renderStatus = ( active) => {
     if ( active === true) {
@@ -54,21 +61,77 @@ class InternalIncident extends Component {
     })
   }
 
+  handleToggle = (event) => {
+    if(event.target.name === 'usernameToggle') {
+      this.setState({
+        username_public: !this.state.username_public
+      });  
+    }
+    else if (event.target.name === 'timedateToggle') {
+      this.setState({
+        timedate_public: !this.state.timedate_public
+      });
+    }
+    else if (event.target.name === 'locationToggle') {
+      this.setState({
+        location_public: !this.state.location_public
+      });
+    }
+    else if (event.target.name === 'typeToggle') {
+      this.setState({
+        type_public: !this.state.type_public
+      });
+    }
+    else if (event.target.name === 'userNotesToggle') {
+      this.setState({
+        user_notes_public: !this.state.user_notes_public
+      });
+    }
+  }
+
+  // need function here to dispatch, I guess
+
   render() {
     return (
       <div className="internalModule">
         <h3>Incident Number: {this.props.incident.client_id}</h3>
         {JSON.stringify(this.props.incident)}
+        {JSON.stringify(this.state)}
         {this.renderStatus( this.props.incident.active)}
         {/* username toggle here */}
+        {this.props.incident.username_public !== undefined &&
+          <ToggleSwitch toggleName="usernameToggle"
+            handleToggle={this.handleToggle} toggleOn={this.props.incident.username_public}
+          />
+        }
         <p>Submitted by: {this.props.incident.username}</p>
         {/* time/date toggle here */}
+        {this.props.incident.timedate_public !== undefined &&
+          <ToggleSwitch toggleName="timedateToggle"
+            handleToggle={this.handleToggle} toggleOn={this.props.incident.timedate_public}
+          />
+        }
         {this.renderTime( this.props.incident.time_submitted)}
         {/* location toggle goes here */}
+        {this.props.incident.location_public !== undefined &&
+          <ToggleSwitch toggleName="locationToggle"
+            handleToggle={this.handleToggle} toggleOn={this.props.incident.location_public}
+          />
+        }
         <p>Location: {this.props.incident.location}</p>
         {/* type toggle goes here */}
+        {this.props.incident.type_public !== undefined &&
+          <ToggleSwitch toggleName="typeToggle"
+            handleToggle={this.handleToggle} toggleOn={this.props.incident.type_public}
+          />
+        }
         <p>Type: {this.props.incident.type}</p>
         {/* toggle for user notes goes here */}
+        {this.props.incident.user_notes_public !== undefined &&
+          <ToggleSwitch toggleName="userNotesToggle"
+            handleToggle={this.handleToggle} toggleOn={this.props.incident.user_notes_public}
+          />
+        }
         <p>User Notes: {this.props.incident.notes}</p>
         <label htmlFor="publicText">
           Text to be displayed at beginning of public post:

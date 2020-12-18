d1c02b99ce59c906c3bbedefb9fad08f2407316e
Franz Cordes-Eklund <franzcordeseklund@gmail.com>
2020-12-18 14:11:45 -0600

    connected router to user. now able to submit new incident reports to database

--- src/components/ReportIncident/ReportIncident.js
+++ src/components/ReportIncident/ReportIncident.js
@@ -5,20 +5,44 @@ import mapStoreToProps from '../../redux/mapStoreToProps';
 class ReportIncident extends Component {
 
     state = {
-        showReport: false
+        showReport: false,
+        type: '',
+        notes: '',
+        location: '',
+        time_submitted: ''
+    }
+
+    componentDidMount = () => {
+      this.clock();
+    }
+
+    clock = () => {
+      setInterval(() => {
+        this.setState({
+          time_submitted : new Date().toLocaleString()
+        })
+      }, 1000)
     }
 
     submitReport = () => {
         console.log('clicked on report incident');
         this.setState( {
             showReport: true
-        })
+        });
+    }
+
+    handleChange = (event, typeParam) => {
+      console.log(event.target.value, typeParam);
+      this.setState( {
+          [typeParam]: event.target.value
+      });
     }
 
     confirmIncident = () => {
-        this.setState( {
-            showReport: false
-        })
+      this.setState( {
+          showReport: false
+      });
+      this.props.dispatch({ type: 'POST_INCIDENT', payload: this.state });
     }
 
   render() {
@@ -27,10 +51,10 @@ class ReportIncident extends Component {
         {this.state.showReport === true ? 
         <div className="editModal">
             <h2>IS THIS CORRECT?</h2>
-            <p>Time/Date:</p>
-            <p>Location:</p>
-            <p>Incident type:</p>
-            <p>Notes:</p>
+            <p>Time/Date: {this.state.time_submitted}</p>
+            <p>Location: {this.state.location}</p>
+            <p>Incident type: {this.state.type}</p>
+            <p>Notes: {this.state.notes}</p>
             <br/>
             <p>Updates on the incident? Y/N</p>
             <p>Sign up for an account Y/N (ANON)</p>
@@ -43,30 +67,32 @@ class ReportIncident extends Component {
     <></>
     }
       <div>
-        <input type="datetime-local" placeholder="TIME/DATE"></input>
+        {JSON.stringify(this.state)}
+        <p>Current Time: <br/>
+        {this.state.time_submitted}</p>
         <br/>
-        <input type="text" placeholder="Location"></input>
+        <input type="text" placeholder="Location" onChange={(event) => this.handleChange(event, 'location')}></input>
         <br/>
-        <input type="checkbox"></input>
+        <input type="checkbox" value="gunshot" onChange={(event) => this.handleChange(event, 'type')}></input>
         <label>Gun shots heard</label>
         <br/>
-        <input type="checkbox"></input>
+        <input type="checkbox" value="stray pet" onChange={(event) => this.handleChange(event, 'type')}></input>
         <label>Stray Pet</label>
         <br/>
-        <input type="checkbox"></input>
+        <input type="checkbox" value="fire" onChange={(event) => this.handleChange(event, 'type')}></input>
         <label>Fire</label>
         <br/>
-        <input type="checkbox"></input>
+        <input type="checkbox" value="vehicle" onChange={(event) => this.handleChange(event, 'type')}></input>
         <label>Vehicle incident</label>
         <br/>
-        <input type="checkbox"></input>
+        <input type="checkbox" value="mental health crisis" onChange={(event) => this.handleChange(event, 'type')}></input>
         <label>Mental health crisis</label>
         <br/>
         <input type="checkbox"></input>
         <label>Other</label>
-        <input type="text"></input>
+        <input type="text" onChange={(event) => this.handleChange(event, 'type')}></input>
         <br/>
-        <textarea placeholder="Additional Notes"></textarea>
+        <textarea placeholder="Additional Notes" onChange={(event) => this.handleChange(event, 'notes')}></textarea>
         <br/>
         <button className="btn" onClick={this.submitReport}>Submit Incident</button>
       </div>

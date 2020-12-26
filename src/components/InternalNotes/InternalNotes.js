eab007da11c9694aa5138c874d73ffdbb462425c
Kelly Danger <kellyanndanger@gmail.com>
2020-12-26 13:27:37 -0600

    input created for internal notes

--- src/components/InternalNotes/InternalNotes.js
+++ src/components/InternalNotes/InternalNotes.js
@@ -3,10 +3,27 @@ import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 
 class InternalNotes extends Component {
+  state = {
+    noteText: ''
+  }
+
+  handleChange = () => {
+    console.log('Handling change');
+    
+  }
+
+  handleSubmit = () => {
+    console.log('CLICKED');
+    
+  }
 
   render (){
     return(
-      <p>This is an internal Note</p>
+      <>
+      <label htmlFor="internalNoteInput">Notes/Updates</label>
+      <input className="internalNoteInput" type="text" placeholder="add an update or note on this incident" onChange={this.handleChange}></input> 
+      <button className="noteSubmit" onClick={this.handleSubmit}>Submit Note</button>
+      </>
     )
 
   }

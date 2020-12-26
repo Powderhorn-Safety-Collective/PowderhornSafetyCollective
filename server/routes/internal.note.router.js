3b0b6a1ac654c475b615547f01efe61a500854c9
Kelly Danger <kellyanndanger@gmail.com>
2020-12-26 13:55:03 -0600

    notes route up and talking to DB

--- server/routes/internal.note.router.js
+++ server/routes/internal.note.router.js
@@ -7,7 +7,13 @@ const {
 
   router.post('/', (req, res) => {
     console.log('POST NOTES', req.body);
-    
-  })
+    const queryText = `INSERT INTO "internal_notes" ("text", "incident_id") VALUES($1, $2)`;
+    pool.query(queryText, [req.body.noteText, req.body.id])
+      .then((result) => res.sendStatus(201))
+      .catch((error) => {
+        console.log('error in post notes', error);
+        res.sendStatus(500);
+      });
+  });
 
   module.exports = router;

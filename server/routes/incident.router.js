232c765636ee897de7a20cfdc50f012a90ca556a
Adam Boerhave <arboerhave@gmail.com>
2020-12-27 14:02:49 -0600

    authorization added for sorting routes

--- server/routes/incident.router.js
+++ server/routes/incident.router.js
@@ -234,7 +234,7 @@ router.put('/assign', (req, res) => {
       });
   });
 
-  //get all incident data for searched incident
+  // get all incident data for searched incident
   router.get('/search/:num', (req, res) => {
     let queryText = `SELECT * FROM "incidents"
     WHERE "client_id" = '${req.params.num}';`;
@@ -277,94 +277,139 @@ router.post('/', (req, res) => {
 
 // below are all the query functions to sort the incident table by column
 router.get('/type', rejectUnauthenticated, (req, res) => {
-  // sort by type
-  const queryText = `SELECT * FROM "incidents" ORDER BY "type";`
-  pool.query(queryText)
+  if (req.user.role == 3) {
+    // sort by type
+    const queryText = `SELECT * FROM "incidents" ORDER BY "type";`
+    pool.query(queryText)
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log(error);
       res.sendStatus(500);
     });
+  }
+  else {
+    res.sendStatus(403);
+  }
 });
 router.get('/notes', rejectUnauthenticated, (req, res) => {
-  // sort by notes
-  const queryText = `SELECT * FROM "incidents" ORDER BY "notes";`
-  pool.query(queryText)
+  if(req.user.role == 3) {
+    // sort by notes
+    const queryText = `SELECT * FROM "incidents" ORDER BY "notes";`
+    pool.query(queryText)
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log(error);
       res.sendStatus(500);
-    });
+    }); 
+  }
+  else {
+    res.sendStatus(403);
+  }
 });
 router.get('/location', rejectUnauthenticated, (req, res) => {
-  // sort by notes
-  const queryText = `SELECT * FROM "incidents" ORDER BY "location";`
-  pool.query(queryText)
+  if (req.user.role == 3) {
+    // sort by notes
+    const queryText = `SELECT * FROM "incidents" ORDER BY "location";`
+    pool.query(queryText)
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log(error);
       res.sendStatus(500);
     });
+  }
+  else {
+    res.sendStatus(403);
+  }
 });
 router.get('/time_submitted', rejectUnauthenticated, (req, res) => {
-  // sort by time_submitted
-  const queryText = `SELECT * FROM "incidents" ORDER BY "time_submitted";`
-  pool.query(queryText)
+  if (req.user.role == 3) {
+    // sort by time_submitted
+    const queryText = `SELECT * FROM "incidents" ORDER BY "time_submitted";`
+    pool.query(queryText)
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log(error);
       res.sendStatus(500);
     });
+  }
+  else {
+    res.sendStatus(403);
+  }
 });
 router.get('/status', rejectUnauthenticated, (req, res) => {
-  // sort by status
-  const queryText = `SELECT * FROM "incidents" ORDER BY "active";`
-  pool.query(queryText)
+  if (req.user.role == 3) {
+    // sort by status
+    const queryText = `SELECT * FROM "incidents" ORDER BY "active";`
+    pool.query(queryText)
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log(error);
       res.sendStatus(500);
     });
+  }
+  else {
+    res.sendStatus(403);
+  }
 });
 router.get('/view_publicly', rejectUnauthenticated, (req, res) => {
-  // sort by view_publicly
-  const queryText = `SELECT * FROM "incidents" ORDER BY "view_publicly";`
-  pool.query(queryText)
+  if (req.user.role == 3) {
+    // sort by view_publicly
+    const queryText = `SELECT * FROM "incidents" ORDER BY "view_publicly";`
+    pool.query(queryText)
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log(error);
       res.sendStatus(500);
     });
+  }
+  else {
+    res.sendStatus(403);
+  }
 });
 router.get('/responder_notes', rejectUnauthenticated, (req, res) => {
-  // sort by responder_notes
-  const queryText = `SELECT * FROM "incidents" ORDER BY "responder_notes";`
-  pool.query(queryText)
+  if (req.user.role == 3) {
+    // sort by responder_notes
+    const queryText = `SELECT * FROM "incidents" ORDER BY "responder_notes";`
+    pool.query(queryText)
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log(error);
       res.sendStatus(500);
     });
+  }
+  else {
+    res.sendStatus(403);
+  }
 });
 router.get('/duplicate_entry', rejectUnauthenticated, (req, res) => {
-  // sort by duplicate_entry
-  const queryText = `SELECT * FROM "incidents" ORDER BY "duplicate_entry";`
-  pool.query(queryText)
+  if(req.user.role == 3) {
+    // sort by duplicate_entry
+    const queryText = `SELECT * FROM "incidents" ORDER BY "duplicate_entry";`
+    pool.query(queryText)
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log(error);
       res.sendStatus(500);
     });
+  }
+  else {
+    res.sendStatus(403);
+  }
 });
 router.get('/client_id', rejectUnauthenticated, (req, res) => {
-  // sort by client_id
-  const queryText = `SELECT * FROM "incidents" ORDER BY "client_id";`
-  pool.query(queryText)
+  if (req.user.role == 3) {
+    // sort by client_id
+    const queryText = `SELECT * FROM "incidents" ORDER BY "client_id";`
+    pool.query(queryText)
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log(error);
       res.sendStatus(500);
     });
+  }
+  else {
+    res.sendStatus(403);
+  }
 });
 // end of table sorting routes
 

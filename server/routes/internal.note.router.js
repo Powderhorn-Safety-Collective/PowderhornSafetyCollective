9eae66561c937f82a2efc17caeaa50d07d60413f
Adam Boerhave <arboerhave@gmail.com>
2020-12-27 15:43:14 -0600

    added authorization to notes routes

--- server/routes/internal.note.router.js
+++ server/routes/internal.note.router.js
@@ -5,29 +5,37 @@ const {
     rejectUnauthenticated,
   } = require('../modules/authentication-middleware');
 
-  // This route sends a new note to the database
-  router.post('/', (req, res) => {
+// This route sends a new note to the database
+router.post('/', rejectUnauthenticated, (req, res) => {
+  if (req.user.role > 1) {
     console.log('POST NOTES', req.body);
     const queryText = `INSERT INTO "internal_notes" ("text", "incident_id") VALUES($1, $2)`;
     pool.query(queryText, [req.body.noteText, req.body.id])
-      .then(() => res.sendStatus(201))
-      .catch((error) => {
-        console.log('error in post notes', error);
-        res.sendStatus(500);
-      });
-  });
+    .then(() => res.sendStatus(201))
+    .catch((error) => {
+      console.log('error in post notes', error);
+      res.sendStatus(500);
+    });
+  }
+  else {
+    res.sendStatus(403);
+  }
+});
 
-  // this route gets the notes from the database
-  router.get('/', (req, res) => {
+// this route gets the notes from the database
+router.get('/', (req, res) => {
+  if (req.user.role > 1) {
     const queryText = `SELECT * FROM "internal_notes" ORDER BY "id" DESC;`;
     pool.query(queryText)
-      .then((result) => {res.send(result.rows)})
-      .catch((error) => {
-        console.log('error in get notes', error);
-        res.sendStatus(500);
-      })
-  })
+    .then((result) => {res.send(result.rows)})
+    .catch((error) => {
+      console.log('error in get notes', error);
+      res.sendStatus(500);
+    });
+  }
+  else {
+    res.sendStatus(403);
+  }
+});
 
-  router
-
-  module.exports = router;
+module.exports = router;

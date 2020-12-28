cd2de0f217981419f7b9373a8a4f4945f90a173c
Kelly Danger <kellyanndanger@gmail.com>
2020-12-28 12:14:23 -0600

    left join

--- server/routes/incident.router.js
+++ server/routes/incident.router.js
@@ -15,8 +15,8 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 
   if(req.user.role > 1) {
     const queryText = `SELECT "incidents"."id", "type", "notes", "location", "time_submitted", "view_publicly", "duplicate_entry", "client_id", "incidents"."username", "username_public", "timedate_public", "location_public", "type_public", "user_notes_public", "text_for_public_display",  "user"."first_name", "active", "assigned_user", "first_name" AS "assigned" FROM "incidents" 
-    JOIN "user" on "user"."id" = "incidents"."assigned_user"
-    ORDER BY "time_submitted" DESC;`
+    LEFT JOIN "user" on "user"."id" = "incidents"."assigned_user"
+    ORDER BY "time_submitted" DESC;`;
     pool.query(queryText)
     .then((results) => {res.send(results.rows)
     console.log('results.rows', results.rows);

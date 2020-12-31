f38456e5f42bcdb90cd4dc7ecd29b9c01883815f
Adam Boerhave <arboerhave@gmail.com>
2020-12-30 20:25:09 -0600

    added sweetalert for updating display text

--- src/redux/sagas/incident.saga.js
+++ src/redux/sagas/incident.saga.js
@@ -1,5 +1,6 @@
 import { put, takeEvery, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
+import swal from 'sweetalert';
 
 // this function grabs all incidents data from the incident table
 // then passes all data along to incidentReducer
@@ -75,9 +76,10 @@ function* editIncident(action) {
  function* updatePublicText(action) {
    console.log('updatePublicText action.payload', action.payload);
    try {
-     yield axios.put('/api/incident/publicText', action.payload);
-     yield put({type: 'GET_INCIDENTS'});
-     alert('Text has been saved.')
+    yield axios.put('/api/incident/publicText', action.payload);
+    yield put({type: 'GET_INCIDENTS'});
+    //  alert('Text has been saved.')
+    swal('Text has been saved', "", "success");
    }
    catch (error) {
      console.log('error from update public text saga', error);

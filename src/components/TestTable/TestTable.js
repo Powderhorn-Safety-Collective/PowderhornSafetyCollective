561fe776611a5e6fbc22618cd67cfd1ccc9ce56a
Franz Cordes-Eklund <franzcordeseklund@gmail.com>
2021-01-04 10:27:56 -0600

    deleted bad code

--- src/components/TestTable/TestTable.js
+++ src/components/TestTable/TestTable.js
@@ -13,7 +13,6 @@ import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
-import PropTypes from 'prop-types';
 
 const useRowStyles = makeStyles({
   root: {
@@ -22,32 +21,6 @@ const useRowStyles = makeStyles({
     },
   },
 });
-
-// function createData(client_id, type, notes, location, time_submitted) {
-//   return {
-//     client_id,
-//     type,
-//     notes,
-//     location,
-//     time_submitted,
-//     // history: [
-//     //   { active: , 
-//     //     public: this.props.incident.view_publicly, 
-//     //     internal_notes: this.notesFunction(), 
-//     //     duplicate: this.props.incident.duplicate_entry, 
-//     //     submitted_by: this.props.incident.username, 
-//     //   },
-//     // ],
-//     history: [
-//         { active: 'No', 
-//           public: 'Yes',  
-//           internal_notes: 'Unknown',  
-//           duplicate: 'No',  
-//           submitted_by: 'Franz',  
-//         },
-//     ],
-//   };
-// }
 
 function Row(props) {
   const { row } = props;
@@ -91,7 +64,7 @@ function Row(props) {
                 <TableBody>
                     <TableRow key={row.id}>
                       <TableCell component="th" scope="row">
-                        {row.active.toString()}
+                        <input defaultValue={row.active.toString()} placeholder={row.active.toString()}></input>
                       </TableCell>
                       <TableCell>{row.view_publicly.toString()}</TableCell>
                       {/* <TableCell>{row.internal_notes}</TableCell> */}
@@ -111,44 +84,6 @@ function Row(props) {
     </React.Fragment>
   );
 }
-
-// Row.propTypes = {
-//   row: PropTypes.shape({
-//     // history: PropTypes.arrayOf(
-//     //   PropTypes.shape({
-//     //     amount: PropTypes.number.isRequired,
-//     //     duplicate: PropTypes.bool.isRequired, 
-//     //     internal_notes: PropTypes.string.isRequired,
-//     //     public: PropTypes.bool.isRequired,
-//     //     submitted_by: PropTypes.string.isRequired,
-//     //   }),
-//     //   ).isRequired,
-//     active: PropTypes.bool,  
-//     duplicate: PropTypes.bool,
-//     internal_notes: PropTypes.string,
-//     public: PropTypes.bool,
-//     submitted_by: PropTypes.string,  
-//     client_id: PropTypes.number,
-//     location: PropTypes.string,
-//     notes: PropTypes.string,
-//     time_submitted: PropTypes.string,
-//     type: PropTypes.string,
-//   }).isRequired,
-// };
-
-// const rows = [
-//   createData(
-//     this.props.incident.client_id, 
-//     this.props.incident.type, 
-//     this.props.incident.notes, 
-//     this.props.incident.location, 
-//     this.renderTime(this.props.incident.time_submitted), 
-//     ),
-//   createData(491729, 'Lost Dog', 'Missing dog, running around barking.', 'Powderhorn Park', 'Noon, yesterday.'),
-//   createData('Eclair', 262, 16.0, 24, 6.0, 3.79),
-//   createData('Cupcake', 305, 3.7, 67, 4.3, 2.5),
-//   createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-// ];
 
 export default function CollapsibleTable(props) {
   return (
